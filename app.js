6abafd9fe1650ad4fc7fd09c139e697a3cbb0f74
davecastaneda <smopo24@gmail.com>
2020-09-07 23:06:02 -0500

    Delted wav files - no longer needed, added js event listener, added data source for html to divs, updated css to allow imgs to be clicked and activate sound

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const drums = new Howl({
-  src: ["./drums/drums.webm", "./drums/drums.mp3"],
-  sprite: {
-    clap: [0, 734.2630385487529],
-    "closed-hihat": [2000, 355.8730158730157],
-    crash: [4000, 1249.410430839002],
-    kick: [7000, 521.2018140589568],
-    "open-hihat": [9000, 922.0634920634918],
-    snare: [11000, 543.5374149659857],
-  },
+    src: ["./drums/drums.webm", "./drums/drums.mp3"],
+    "sprite": {
+        "clap": [0, 734.2630385487529],
+        "closed-hihat": [2000, 355.8730158730157],
+        "crash": [4000, 1249.410430839002],
+        "kick": [7000, 521.2018140589568],
+        "open-hihat": [9000, 922.0634920634918],
+        "snare": [11000, 543.5374149659857]
+    }
 });
 
-drums.play("kick");
+const drumkit = document.querySelector(".drumkit");
+
+drumkit.addEventListener('click', () => {
+    if (event.target.classList.contains('pad')) {
+        let soundToPlay = event.target.dataset.sound;
+        drums.play(soundToPlay);
+    }
+});
